b891e3c7b54ad85cb3e9aa910a53b4ce387d74d9
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-31 10:46:36 +0000

    31 Aug 2023, 16:16

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -16,13 +16,13 @@ const Form = () => {
 
   return (
     <>
-      <h1 className="flex justify-center text-4xl p-4">
+      <h1 className="flex justify-center text-4xl p-4 ">
         <div style={{ position: 'absolute', top: '.5rem', right: '.5rem' }}>
           {currentStepIndex + 1}/{steps.length}
         </div>
         <form>{Step}</form>
       </h1>
-      <div className="flex justify-center gap-2 ">
+      <div className="flex justify-center gap-2 align-bottom ">
         <button
           className=" border p-2 border-spacing-2 rounded-2xl "
           onClick={next}
