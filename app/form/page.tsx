32fd3b3575ec96648293331b2edfd2eda1c71bf5
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-31 10:36:38 +0000

    31 Aug 2023, 16:06

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -5,7 +5,7 @@ import Form1 from '@/src/components/Form1';
 import Form2 from '@/src/components/Form2';
 
 const Form = () => {
-  const { currentStepIndex, back, goTo, next, Step } = useForm([
+  const { currentStepIndex, back, goTo, next, Step, steps } = useForm([
     <div key="1">
       <Form1 />
     </div>,
@@ -17,7 +17,9 @@ const Form = () => {
   return (
     <>
       <h1 className="flex justify-center text-4xl p-4">
-        <div style={{ position: 'absolute', top: '.5', right: '.5' }}></div>
+        <div style={{ position: 'absolute', top: '.5', right: '.5' }}>
+          {currentStepIndex + 1}/{steps.length}
+        </div>
         <form>{Step}</form>
       </h1>
       <button onClick={next}>Next</button>
