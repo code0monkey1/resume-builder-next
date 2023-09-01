0ac873339dded99af97a5cc396fad5448d52047a
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-01 06:58:36 +0000

    1 Sept 2023, 12:28

--- src/components/Form2.tsx
+++ src/components/Form2.tsx
@@ -4,7 +4,16 @@ import React from 'react';
 type Props = {};
 
 const Form2 = (props: Props) => {
-  return <div>Form2</div>;
+  <div>
+    <label>Street</label>
+    <input autoFocus required type="text" />
+    <label>City</label>
+    <input required type="text" />
+    <label>State</label>
+    <input required type="text" />
+    <label>Zip</label>
+    <input required type="text" />
+  </div>;
 };
 
 export default Form2;
