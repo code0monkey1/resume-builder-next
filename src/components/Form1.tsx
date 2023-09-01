753db6ce32eaaba2678e2cde1f4a559184fe1603
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-01 07:20:30 +0000

    1 Sept 2023, 12:50

--- src/components/Form1.tsx
+++ src/components/Form1.tsx
@@ -1,34 +1,37 @@
 'use client';
 import React from 'react';
+import FormWrapper from './FormWrapper';
 type Props = {};
 
 const Form1 = (props: Props) => {
   return (
     <>
-      <div className="flex flex-col border border-black  p-2">
-        <label>First Name</label>
-        <input
-          className=" border w-fit p-2"
-          placeholder="enter your name"
-          type="text"
-          autoFocus
-          required
-        />
-        <label>Last Name</label>
-        <input
-          className=" border  w-fit p-2"
-          placeholder="enter your name"
-          type="text"
-          required
-        />
-        <label>Age</label>
-        <input
-          className=" border w-fit p-2"
-          placeholder="enter your name"
-          type="text"
-          required
-        />
-      </div>
+      <FormWrapper title="Personal Information">
+        <div className="flex flex-col border border-black  p-2">
+          <label>First Name</label>
+          <input
+            className=" border w-fit p-2"
+            placeholder="enter your name"
+            type="text"
+            autoFocus
+            required
+          />
+          <label>Last Name</label>
+          <input
+            className=" border  w-fit p-2"
+            placeholder="enter your name"
+            type="text"
+            required
+          />
+          <label>Age</label>
+          <input
+            className=" border w-fit p-2"
+            placeholder="enter your name"
+            type="text"
+            required
+          />
+        </div>
+      </FormWrapper>
     </>
   );
 };
--- src/components/Form2.tsx
+++ src/components/Form2.tsx
@@ -1,20 +1,26 @@
 'use client';
 import React from 'react';
+import FormWrapper from './FormWrapper';
 
 type Props = {};
 
 const Form2 = (props: Props) => {
   return (
-    <div>
-      <label>Street</label>
-      <input autoFocus required type="text" />
-      <label>City</label>
-      <input required type="text" />
-      <label>State</label>
-      <input required type="text" />
-      <label>Zip</label>
-      <input required type="text" />
-    </div>
+    <FormWrapper title="Address">
+      <div
+        className="flex flex-col
+      "
+      >
+        <label>Street</label>
+        <input autoFocus required type="text" />
+        <label>City</label>
+        <input required type="text" />
+        <label>State</label>
+        <input required type="text" />
+        <label>Zip</label>
+        <input required type="text" />
+      </div>
+    </FormWrapper>
   );
 };
 
