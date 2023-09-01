63e9f36a30a19656b57be4d815a6cbd0d6de1d38
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-01 05:57:53 +0000

    1 Sept 2023, 11:27

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -15,12 +15,15 @@ const Form = () => {
   ]);
 
   return (
-    <div className="relative bg-white border border-solid border-black p-8 m-4 rounded-md font-mono">
-      FORM
-      <form>
-        <h1 className="absolute top-2 right-2 ">2 / 3</h1>
-      </form>
+    <div>
+      <h1>Form</h1>
     </div>
+    // <div className="relative bg-white border border-solid border-black p-8 m-4 rounded-md font-mono">
+    //   FORM
+    //   <form>
+    //     <h1 className="absolute top-2 right-2 ">2 / 3</h1>
+    //   </form>
+    // </div>
   );
 };
 export default Form;
