769af452cf16537132cbde19f0950cabc98732f1
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-01 07:01:08 +0000

    1 Sept 2023, 12:31

--- src/components/Form2.tsx
+++ src/components/Form2.tsx
@@ -4,16 +4,18 @@ import React from 'react';
 type Props = {};
 
 const Form2 = (props: Props) => {
-  <div>
-    <label>Street</label>
-    <input autoFocus required type="text" />
-    <label>City</label>
-    <input required type="text" />
-    <label>State</label>
-    <input required type="text" />
-    <label>Zip</label>
-    <input required type="text" />
-  </div>;
+  return (
+    <div>
+      <label>Street</label>
+      <input autoFocus required type="text" />
+      <label>City</label>
+      <input required type="text" />
+      <label>State</label>
+      <input required type="text" />
+      <label>Zip</label>
+      <input required type="text" />
+    </div>
+  );
 };
 
 export default Form2;
---
+++ src/components/Form3.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type Props = {};
+
+const Form3 = (props: Props) => {
+  return (
+    <div>
+      <label>Email</label>
+      <input type="text" />
+      <label>Password</label>
+      <input type="text" />
+    </div>
+  );
+};
+
+export default Form3;
