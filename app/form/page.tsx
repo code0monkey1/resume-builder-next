4672b668292dc8f57f756cc99a3fea262956b8f0
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-01 05:38:49 +0000

    1 Sept 2023, 11:08

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -7,15 +7,15 @@ import Form2 from '@/src/components/Form2';
 const Form = () => {
   const { currentStepIndex, back, goTo, next, Step, steps } = useForm([
     <div key="1">
-      <Form1 />
+      <h1>Hello</h1>
     </div>,
     <div key="2">
-      <Form2 />
+      <h1>There</h1>
     </div>,
   ]);
 
   return (
-    <div className="relative bg-white border border-solid border-black p-8 m-4 rounded-md font-extralight">
+    <div className="relative bg-white border border-solid border-black p-8 m-4 rounded-md font-mono">
       <form>
         <h2>{Step}</h2>
       </form>
