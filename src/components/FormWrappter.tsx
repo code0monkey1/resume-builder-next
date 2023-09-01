7afe512b977807bdbd4f1d4fafa8dc2526bdb899
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-01 07:10:04 +0000

    1 Sept 2023, 12:40

--- src/components/FormWrappter.tsx
+++ src/components/FormWrappter.tsx
@@ -8,7 +8,8 @@ type Props = {
 const FormWrappter = ({ title, children }: Props) => {
   return (
     <div>
-      <h2 className="text-center mt-0 mb-8"></h2>
+      <h2 className="text-center mt-0 mb-8">{title}</h2>
+      <div className="grid gap-x-4 gap-y-2 justify-start"></div>
     </div>
   );
 };
