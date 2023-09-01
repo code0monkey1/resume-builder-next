41e911d5af3261b351757d794aea214d3e7af668
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-01 06:47:02 +0000

    1 Sept 2023, 12:17

--- src/components/Form1.tsx
+++ src/components/Form1.tsx
@@ -3,7 +3,16 @@ import React from 'react';
 type Props = {};
 
 const Form1 = (props: Props) => {
-  return <div>Form1</div>;
+  return (
+    <>
+      <label>First Name</label>
+      <input type="text" autoFocus></input>
+      <label>Last Name</label>
+      <input type="text"></input>
+      <label>Age</label>
+      <input type="number" />
+    </>
+  );
 };
 
 export default Form1;
