14453f30b760769c9de8ba97f0e0c1b095f8252a
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-01 05:47:26 +0000

    1 Sept 2023, 11:17

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="bg-white">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} `}>{children}</body>
     </html>
   );
 }
