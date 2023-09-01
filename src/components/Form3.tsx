2f04b87a559b6c11f8f2ae601ec47c25475e9c5f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-01 07:32:31 +0000

    1 Sept 2023, 13:02

--- src/components/Form3.tsx
+++ src/components/Form3.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import FormWrapper from './FormWrapper';
 
 type Props = {};
 
 const Form3 = (props: Props) => {
   return (
-    <div>
+    <FormWrapper title="Personal Info">
       <label>Email</label>
       <input type="text" />
       <label>Password</label>
       <input type="text" />
-    </div>
+    </FormWrapper>
   );
 };
 
