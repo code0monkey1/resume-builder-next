f7068a0dc0f2ae4ac86637b9ca55011ef3708702
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-02 12:42:39 +0000

    2 Sept 2023, 18:12

--- src/components/Form2.tsx
+++ src/components/Form2.tsx
@@ -1,4 +1,6 @@
 'use client';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import React from 'react';
 import FormWrapper from './FormWrapper';
 
@@ -7,14 +9,14 @@ type Props = {};
 const Form2 = (props: Props) => {
   return (
     <FormWrapper title="Address">
-      <label>Street</label>
-      <input autoFocus required type="text" />
-      <label>City</label>
-      <input required type="text" />
-      <label>State</label>
-      <input required type="text" />
-      <label>Zip</label>
-      <input required type="text" />
+      <Label>Street</Label>
+      <Input autoFocus required type="text" />
+      <Label>City</Label>
+      <Input required type="text" />
+      <Label>State</Label>
+      <Input required type="text" />
+      <Label>Zip</Label>
+      <Input required type="text" />
     </FormWrapper>
   );
 };
