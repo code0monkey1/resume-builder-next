197b499f9f1c6066e37699f585644eaf9e7ab58b
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-09-02 13:35:18 +0000

    2 Sept 2023, 19:05

--- src/components/FormWrapper.tsx
+++ src/components/FormWrapper.tsx
@@ -8,7 +8,7 @@ type Props = {
 const FormWrapper = ({ title, children }: Props) => {
   return (
     <div>
-      <h1 className="text-center mt-0 mb-8">{title}</h1>
+      <h1 className="text-center mt-4 mb-8">{title}</h1>
       <div className="grid grid-cols-auto grid-cols-min-auto grid-cols-max-400px gap-x-4 gap-y-2 justify-start">
         {children}
       </div>
